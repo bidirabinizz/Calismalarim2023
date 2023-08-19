1e66a5efa400f16bc7659ba76deab821d8573ad2
Bıdır Abiniz <40939231+bidirabinizz@users.noreply.github.com>
2023-08-19 12:57:27 +0300

    19.08 sum

--- Api-calismasi/api/src/App.js
+++ Api-calismasi/api/src/App.js
@@ -1,14 +1,22 @@
+import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import UserListCard from "./components/UserListCard";
 
 function App() {
   const [users, setUsers] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
 
+  let userURL = "https://jsonplaceholder.typicode.com/users";
+
+  useEffect(() => {
+    veriYukle();
+  }, []);
+
   function veriYukle() {
     axios
-      .get("https://jsonplaceholder.typicode.com/users")
+      .get(userURL)
       .then((item) => {
         console.log(item.data);
         setUsers(item.data);
@@ -20,13 +28,22 @@ function App() {
         setLoading(false);
       });
   }
+
   return (
-    <div>
+    <div className="mainDiv1">
       <h1>Kullanıcılar</h1>
-      {error && <h2>Sayfa yüklenemedi</h2>}
-      {loading && <h2>Yükleniyor ...</h2>}
+      {error && <h2>Sayfa Yüklenemedi...</h2>}
+      {loading && <h2>Yükleniyor...</h2>}
       {users.map((item) => (
-        <h4>{item.name}</h4>
+        <UserListCard
+          username={item.name}
+          mail={item.email}
+          company={item.company.name}
+          location={item.address.street}
+          phone={item.phone}
+          loginname={item.username}
+          website={item.website}
+        ></UserListCard>
       ))}
     </div>
   );
---
+++ Api-calismasi/api/src/components/UserListCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./UserListCard.css";
+function UserListCard({
+  loginname,
+  username,
+  mail,
+  company,
+  location,
+  phone,
+  website,
+}) {
+  return (
+    <div className="usercardMain">
+      <h6 className="usernameh6">Username : {loginname}</h6>
+      <h2 className="usercardName">{username}</h2>
+      <h5>{mail}</h5>
+
+      <label>Company: {company}</label>
+      <br />
+      <label>Location : {location}</label>
+      <br />
+      <label>Phone : {phone}</label>
+      <br />
+      <a className="usercardAHref" href={website}>
+        {website}
+      </a>
+    </div>
+  );
+}
+
+export default UserListCard;
